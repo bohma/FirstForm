88d5d4e58c72d2fd352fadf7b8aa5745e1f43897
Bogdan <akela960@gmail.com>
2019-03-17 19:34:58 +0200

    abc

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -1,12 +1,42 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.jws.WebParam;
 
 @Controller
 public class MainController {
-    @GetMapping("/tst")
-    public String getTestPage(){
+    @GetMapping("/test")
+    public String getTestPage() {
         return "test";
+    }
+
+    @GetMapping("/first")
+    public String getFirstSite() {
+        return "first";
+    }
+
+    @GetMapping("/second")
+    public String getSecondSite() {
+        return "second";
+    }
+
+    @PostMapping("/zoho")
+    public String postZoho(@RequestParam String email,
+                           @RequestParam String lname,
+                           @RequestParam String company,
+                           @RequestParam String city,
+                           Model model){
+
+        model.addAttribute("email",email);
+        model.addAttribute("lname",lname);
+        model.addAttribute("company",company);
+        model.addAttribute("city",city);
+
+        return "zoho";
     }
 }
